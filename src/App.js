73ea91f34ad4158d8613170923a5b6f3af971184
Scott Allan <sallan306@gmail.com>
2019-01-14 23:45:19 -0600

    almost done, reshuffling doesnt work

--- src/App.js
+++ src/App.js
@@ -1,26 +1,72 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import FriendCard from "./components/FriendCard";
+import Wrapper from "./components/Wrapper";
+import Title from "./components/Title";
+import friends from "./friends.json";
 
 class App extends Component {
+  // Setting this.state.friends to the friends json array
+  state = {
+    friends,
+    points: 0
+  };
+
+  removeFriend = (id) => {
+    // Filter this.state.friends for friends with an id not equal to the id being removed
+    // const friends = this.state.friends.filter(friend => friend.id === id);
+    // Set this.state.friends equal to the new friends array
+
+
+
+
+
+
+    this.setState({ friends });
+    if (friends[id-1].clicked === "false" && this.state.points !== "GAME OVER") {
+      friends[id-1].clicked = "true";
+      this.setState({ points: this.state.points + 1 });
+      
+      var currentIndex = friends.length, temporaryValue, randomIndex;
+
+      // While there remain elements to shuffle...
+      while (0 !== currentIndex) {
+    
+        // Pick a remaining element...
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+    
+        // And swap it with the current element.
+        temporaryValue = friends[currentIndex];
+        friends[currentIndex] = friends[randomIndex];
+        friends[randomIndex] = temporaryValue;
+        
+      }
+    }
+    else {
+      this.setState({ points: "GAME OVER" });
+    }
+
+  };
+
+
+  // Map over this.state.friends and render a FriendCard component for each friend object
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Wrapper>
+        <Title>{this.state.points}</Title>
+        {this.state.friends.map(friend => (
+          <FriendCard
+            removeFriend={this.removeFriend}
+            id={friend.id}
+            key={friend.id}
+            name={friend.name}
+            image={friend.image}
+            occupation={friend.occupation}
+            location={friend.location}
+            clicked={friend.clicked}
+          />
+        ))}
+      </Wrapper>
     );
   }
 }
